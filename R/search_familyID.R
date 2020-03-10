66624b5caf315daec631e992d06fa1c39f5aa7b3
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-03-10 20:34:37 +0800

    Add files via upload

---
+++ R/search_familyID.R
@@ -0,0 +1,44 @@
+##' @title Search family IDs
+##' @description Search family IDs via family name, supports Latin and Chinese names
+##' @rdname search_familyID
+##' @name search_familyID
+##' @param query \code{character} Family name, or part of family name, supports Latin and Chinese names
+##' @param mc.cores The number of cores to use, i.e. at most how many child processes will be run simultaneously. The option is initialized from environment variable MC_CORES if set. Must be at least one, and parallelization requires at least two cores.
+##' @param apiKey \code{string} You need to apply for the apiKey from \url{http://col.especies.cn/api/document} to run this function
+##' @return dataframe
+##' @author Liuyong Ding
+##' @importFrom jsonlite fromJSON
+##' @importFrom rlist list.stack
+##' @importFrom tibble tibble
+##' @importFrom pbmcapply pbmcmapply
+##' @details Visit the website \url{http://col.especies.cn/api/document} for more details
+##' @examples
+#' \dontrun{
+#' apiKey <- "your apiKey"
+#' search_familyID(query = "Cyprinidae",apiKey = apiKey)
+#' queries <- c("Rosaceae","Cyprinidae")
+#' ss <- search_familyID(query = queries,apiKey = apiKey)
+#' }
+##' @export
+search_familyID <- function(query = NULL,mc.cores = 2,apiKey=NULL) {
+  cat(sprintf("last Update: %s",Sys.Date()),sep = "\n")
+  if(length(query) == 1){
+    x <- familyID(query,apiKey)
+    x <- tibble(family = query,familyIDs = x)
+    x$download.date <- as.Date(Sys.time())
+
+  } else {
+    x <- pbmcmapply(query,familyID,apiKey)
+    x <- tibble(family = query,familyIDs = x,mc.cores = mc.cores)
+    x$download.date <- as.Date(Sys.time())
+  }
+  return(x)
+}
+
+familyID <- function(query = NULL,apiKey = NULL) {
+  query <- as.character(query)
+  url <- paste0('http://www.sp2000.org.cn/api/family/familyName/familyID/', query, '/', apiKey)
+  x <- fromJSON(url,flatten = TRUE)
+  return(x)
+}
+
