a96e695f4dc66cbbe84c17a2fe7678d746b03148
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-03-19 20:22:21 +0800

    Update open_URL.R

--- R/open_URL.R
+++ R/open_URL.R
@@ -1,15 +1,15 @@
 ##' @title Opening the webpage with query
-##' @description  Query with the accceptedName,scientificName,chineseName and commonName", returning the webpage that meets the criteria
+##' @description  Query with the accceptedName,scientificName,chineseName and commonName", returning the webpage that meets the criteria.
 ##' @rdname open_URL
 ##' @name open_URL
-##' @param query \code{string} the accceptedName,scientificName,chineseName or commonName
-##' @param name \code{string} name=c("accceptedName","scientificName","chineseName","commonName"),the default value is "accceptedName"
-##' @param language string currently only two languages are supported: Chinese and English,language=c("en","zh"), the default value is "en"
+##' @param query \code{string} the accceptedName,scientificName,chineseName or commonName.
+##' @param name \code{string} name=c("accceptedName","scientificName","chineseName","commonName"),the default value is "accceptedName".
+##' @param language string currently only two languages are supported: Chinese and English,language=c("en","zh"), the default value is "en".
 ##' @author Liuyong Ding
-##' @details Visit the website \url{http://sp2000.org.cn/pageservices/document} for more details
+##' @details Visit the website \url{http://sp2000.org.cn/pageservices/document} for more details.
 ##' @importFrom utils browseURL
 ##' @examples
-##'\dontrun{
+##'\donttest{
 ##' open_URL(query = "Anguilla marmorata",name = "scientificName",language = 'en')
 ##' open_URL(query = "Anguilla marmorata",name = "scientificName",language = 'zh')
 ##' }
