2f9c52933143522073f5987672ff1226aaa1cdaa
Liuyong Ding <33018532+Otoliths@users.noreply.github.com>
2020-05-22 22:06:13 +0800

    Add files via upload

--- R/download_CoLChina.R
+++ R/download_CoLChina.R
@@ -1,10 +1,9 @@
 #' @title Download 'Catalogue of Life China': Annual Checklist
-#' @description Organized by the Biodiversity Committee of Chinese Academy of Sciences, 'Catalogue of Life China' 2019 Annual Checklist edition has been compiled by 'Species 2000' China Node. It is funded by the Strategic Priority Research Program of the Chinese Academy of Sciences (Grant No. XDA19050202). It is released at Beijing on May, 2019.
+#' @description  Organized by the Biodiversity Committee of Chinese Academy of Sciences (BC-CAS), Catalogue of Life China Annual Checklist edition has been compiled by Species 2000 China Node.
 #' @rdname download_CoLChina
 #' @name download_CoLChina
-#' @param version \code{integer} Release version of annual checklist,the default value is 2019.
-#' @param format \code{character} Release version of format,the default value is zip.
-#' @param download \code{logic} TRUE or FALSE,the default value is FALSE.
+#' @param version \code{integer} Release version of annual checklist,the default value is 2020.
+#' @param OS \code{character} Supported operating system,c("Mac", "Ubuntu" ,"Windows"),the default value is "Mac".
 #' @param dir a non-empty character vector giving the directory name by user,the default value is dir = tempdir(),see [tempdir()] for details.
 #' @return URL
 #' @author Liuyong Ding
@@ -13,21 +12,29 @@
 #' @importFrom utils browseURL
 #' @examples
 #' \donttest{
-#' dir <- tempdir()
-#' download_CoLChina(version = "2019",format = "zip",download = TRUE, dir = dir)
+#' dir <- "data/"
+#' download_CoLChina(version = "2020",OS = "Mac", dir = dir)
 #' }
 #' @export
-download_CoLChina <- function(version = "2019",format = "zip",download = FALSE,dir = tempdir()) {
+download_CoLChina <- function(version = "2020", OS = "Mac", dir = tempdir()) {
   cat(sprintf("Download the date: %s",Sys.Date()),sep = "\n")
-  version <- match.arg(version, 2018:2019)
-  format <- match.arg(format, c("zip", "iso"))
-  url <- paste0('http://sp2000.org.cn/CoL/CoLChina',version, '.',format)
-  if (download) {
-    outfile <- sub(".*/", "", url)
-    download.file(url, file.path(dir,outfile))
-    cat(sprintf("Download path: %s",dir),sep = "\n")
+  version <- match.arg(version, 2018:2020)
+  OS <- match.arg(OS, c("Mac", "Ubuntu" ,"Windows"))
+  if (version %in% c("2018","2019") ){
+    url <- paste0('http://sp2000.org.cn/CoL/CoLChina',version, '.',"iso")
   }
-  browseURL("http://sp2000.org.cn/download")
-  invisible(url)
+  if (version == "2020" & OS == "Mac"){
+    url <- paste0('http://sp2000.org.cn/CoL/CoLChina',version,"-v20.1.12_mac_64", '.',"iso")
+  }
+  if (version == "2020" & OS == "Ubuntu"){
+    url <- paste0('http://sp2000.org.cn/CoL/CoLChina',version,"-v20.1.12_linux_64", '.',"iso")
+  }
+  if (version == "2020" & OS == "Windows"){
+    url <- paste0('http://sp2000.org.cn/CoL/CoLChina',version,"-v20.1.12_win_x86_64", '.',"iso")
+  }
+  outfile <- sub(".*/", "", url)
+  download.file(url, file.path(dir,outfile))
+  cat(sprintf("Download path: %s",dir),sep = "\n")
+  browseURL(url)
 }
 
